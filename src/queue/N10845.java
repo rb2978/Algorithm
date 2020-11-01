d0b2102dabd2a31189c85c0a5e02e81162458cc3
h2919 <h2919@DESKTOP-4GR07H7>
2020-11-01 22:35:47 +0900

    f

---
+++ src/queue/N10845.java
@@ -0,0 +1,12 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class N10845 {
+	public static void main(String[] args) {
+		Queue<Integer> q = new LinkedList<>();
+		String[] str = new String[1];
+	    
+	}
+}
